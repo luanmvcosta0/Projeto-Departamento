4ad2398327ce00cfa5b6d36ba9b98e8f33c28790
luanmvcosta0 <luanmvcosta@mgail.com>
2024-11-11 20:19:34 -0300

    Adicionando o endpoint findAll

---
+++ src/main/java/com/devsuperior/userdept/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.devsuperior.userdept.controller;
+
+import com.devsuperior.userdept.entities.User;
+import com.devsuperior.userdept.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository repository;
+
+    @GetMapping
+    public List<User> findAll() {
+        List<User> result = repository.findAll();
+        return result;
+    }
+
+
+}
